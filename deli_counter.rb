d6b2b4b9831f820f075a7ed428a25879c1166405
Rachel <racheljhumes@gmail.com>
2022-12-13 11:45:16 -0700

    commit tree 0d6eabecfeb8aa027e6ab9f37049974de87f193c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line deli 
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, index|
+      current_line << " #{index}. #{person}"
+    end
+    puts current_line
+  end
+end
